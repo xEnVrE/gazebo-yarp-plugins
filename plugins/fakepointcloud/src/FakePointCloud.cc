8c5f4f5e0af863d6b7890e99d61cb50b3830fbe2
Nicola Piga <nicolapiga@gmail.com>
2018-01-08 22:09:53 +0100

    start development of Fake Point Cloud plugin

---
+++ plugins/fakepointcloud/src/FakePointCloud.cc
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2018 Fondazione Istituto Italiano di Tecnologia iCub Facility
+ * Authors: see AUTHORS file.
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or any later version, see LGPL.TXT or LGPL3.TXT
+ */
+
+#include "FakePointCloud.hh"
+
+#include <gazebo/physics/Model.hh>
+#include <gazebo/common/Events.hh>
+#include <gazebo/physics/PhysicsIface.hh>
+#include <gazebo/physics/World.hh>
+
+#include <GazeboYarpPlugins/common.h>
+
+#include <yarp/os/Log.h>
+#include <yarp/os/LogStream.h>
+
+#include <boost/bind.hpp>
+
+#include <string>
+
+GZ_REGISTER_MODEL_PLUGIN(gazebo::GazeboYarpFakePointCloud)
+
+namespace gazebo {
+
+void GazeboYarpFakePointCloud::Load(gazebo::physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+{    
+    // Store pointer to the model
+    m_model = _parent;
+
+    // Store pointer to the world
+    m_world = m_model->GetWorld();
+    
+    // Check yarp network availability
+    if (!m_yarp.checkNetwork(GazeboYarpPlugins::yarpNetworkInitializationTimeout)) {
+        yError() << "GazeboYarpFakePointCloud::Load error: yarp network does not seem to be available, is the yarpserver running?";
+        return;
+    }
+
+    // Clear last update timen
+    m_lastUpdateTime = gazebo::common::Time(0.0);
+    
+    // Listen to the update event
+    auto worldUpdateBind = boost::bind(&GazeboYarpFakePointCloud::OnWorldUpdate, this);
+    m_worldUpdateConnection = gazebo::event::Events::ConnectWorldUpdateBegin(worldUpdateBind);
+}
+
+void GazeboYarpFakePointCloud::OnWorldUpdate()
+{    
+    // Get current time
+#if GAZEBO_MAJOR_VERSION >= 8
+    gazebo::common::Time currentTime = m_world->SimTime();
+#else
+    gazebo::common::Time currentTime = m_world->GetSimTime();
+#endif
+    
+    // Update the fake point cloud if a period is elapsed
+    if(currentTime - m_lastUpdateTime >= 1.0) {
+	gazebo::common::Time diff = currentTime - m_lastUpdateTime;
+	yInfo() << diff.Float();
+
+	// Store current time for next update
+	m_lastUpdateTime = currentTime;
+    }
+}
+    
+}
