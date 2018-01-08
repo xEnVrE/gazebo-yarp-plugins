1fe65f78c70bf79a83623f3b089c3bb96525858c
Nicola Piga <nicolapiga@gmail.com>
2018-01-08 22:09:53 +0100

    start development of Fake Point Cloud plugin

---
+++ plugins/fakepointcloud/include/gazebo/FakePointCloud.hh
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2018 Fondazione Istituto Italiano di Tecnologia iCub Facility
+ * Authors: see AUTHORS file.
+ * CopyPolicy: Released under the terms of the LGPLv2.1 or any later version, see LGPL.TXT or LGPL3.TXT
+ */
+
+#ifndef GAZEBOYARP_FAKEPOINTCLOUD_HH
+#define GAZEBOYARP_FAKEPOINTCLOUD_HH
+
+#include <gazebo/common/Plugin.hh>
+
+#include <yarp/os/Network.h>
+
+namespace gazebo
+{
+    /// \class GazeboFakePointCloud
+    /// Gazebo Plugin simulating point cloud acqusition using
+    /// the VCG library.
+    ///
+    /// ADD FURTHER EXPLANATIONS
+
+    class GazeboYarpFakePointCloud : public ModelPlugin
+    {
+    public:
+	void Load(gazebo::physics::ModelPtr, sdf::ElementPtr);
+	void OnWorldUpdate();
+	
+    private:
+	yarp::os::Network m_yarp;
+	
+	gazebo::physics::ModelPtr m_model;
+	gazebo::physics::WorldPtr m_world;	
+	gazebo::event::ConnectionPtr m_worldUpdateConnection;
+	gazebo::common::Time m_lastUpdateTime;
+    };
+}
+
+#endif
