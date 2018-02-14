29fc44482aa108d56e5e6269dcefb8593aa9c3cf
Nicola Piga <nicolapiga@gmail.com>
2018-02-14 10:47:10 +0100

    send contact points over a iCub::skinDynLib::skinContactList port

--- plugins/skin/src/Skin.cc
+++ plugins/skin/src/Skin.cc
@@ -11,9 +11,8 @@
 #include <gazebo/sensors/sensors.hh>
 
 // ignition
-// #include <ignition/math/Pose3.hh>
-// #include <ignition/math/Vector3.hh>
-// #include <ignition/math/Quaternion.hh>
+#include <ignition/math/Pose3.hh>
+#include <ignition/math/Vector3.hh>
 
 // GazeboYarpPlugins
 #include <GazeboYarpPlugins/common.h>
@@ -24,6 +23,7 @@
 #include <yarp/os/LogStream.h>
 #include <yarp/os/Value.h>
 #include <yarp/os/Searchable.h>
+#include <yarp/math/FrameTransform.h>
 
 // icub-main
 #include <iCub/skinDynLib/skinContact.h>
@@ -77,6 +77,56 @@ void GazeboYarpSkin::Load(gazebo::physics::ModelPtr _parent, sdf::ElementPtr _sd
         return;
     }
     m_whichHand = whichHandValue.asString();
+
+    // Prepare properties for the Encoders
+    yarp::os::Property propEnc;
+    propEnc.put("device", "remote_controlboard");
+    propEnc.put("remote", "/icubSim/" + m_whichHand + "_arm");
+    propEnc.put("local", "/gazebo_yarp_skin/encoder/" + m_whichHand + "_arm");
+    ok = m_drvArmEnc.open(propEnc);
+    if (!ok) {
+        yError() << "GazeboYarpSkin::Load error:"
+		 << "unable to open the Remote Control Board driver.";
+	return;
+    }
+
+    // Try to retrieve the view
+    m_drvArmEnc.view(m_iEnc);
+    if (!ok || m_iEnc == 0) {
+        yError() << "GazeboYarpSkin::Load error:"
+		 << "unable to retrieve the Encoders view.";
+	return;
+    }
+
+    // Prepare properties for the FrameTransformClient
+    yarp::os::Property propTfClient;
+    propTfClient.put("device", "transformClient");
+    propTfClient.put("local", "/gazebo_yarp_skin/" + m_whichHand + "_hand/transformClient");
+    propTfClient.put("remote", "/transformServer");
+	
+    // try to open the driver
+    ok = m_drvTransformClient.open(propTfClient);
+    if (!ok) {
+        yError() << "GazeboYarpSkin::Load error:"
+		 << "unable to open the FrameTransformClient driver.";
+	return;
+    }
+    
+    // Try to retrieve the view
+    ok = m_drvTransformClient.view(m_tfClient);
+    if (!ok || m_tfClient == 0) {
+        yError() << "GazeboYarpSkin::Load error:"
+		 << "unable to retrieve the FrameTransformClient view.";
+	return;
+    }
+
+    // Retrieve the pose of the root frame of the robot
+    ok = RetrieveRobotRootFrame(m_inertialToRobot);
+    if (!ok) {
+	yError() << "GazeboYarpSkin:Load error:"
+		 << "unable to get the pose of the root link of the robot.";
+	return;
+    }
     
     // Configure all the contact sensors
     ok = ConfigureAllContactSensors();
@@ -95,6 +145,41 @@ void GazeboYarpSkin::Load(gazebo::physics::ModelPtr _parent, sdf::ElementPtr _sd
     // listen to the update event
     auto worldUpdateBind = boost::bind(&GazeboYarpSkin::OnWorldUpdate, this);
     m_worldUpdateConnection = gazebo::event::Events::ConnectWorldUpdateBegin(worldUpdateBind);
+}
+
+bool GazeboYarpSkin::RetrieveRobotRootFrame(ignition::math::Pose3d &pose)
+{
+    // Get the pose of the root frame of the robot
+    // TODO: get source and target from configuration file
+    yarp::sig::Matrix inertialToRobot(4,4);
+    std::string source = "/inertial";
+    std::string target = "/iCub/frame";
+
+    bool ok = false;
+    double t0 = yarp::os::SystemClock::nowSystem();
+    while (yarp::os::SystemClock::nowSystem() - t0 < 10.0)
+    {
+	// this might fail if the gazebo pluging
+	// publishing the pose is not started yet
+	if (m_tfClient->getTransform(target, source, inertialToRobot))
+	{
+	    ok = true;
+	    break;
+	}
+	yarp::os::SystemClock::delaySystem(1.0);
+    }
+
+    // Convert to ignition::math::Pose3d
+    yarp::math::FrameTransform frame;
+    frame.fromMatrix(inertialToRobot);
+
+    yarp::math::FrameTransform::Translation_t  &pos = frame.translation;
+    yarp::math::Quaternion &quat = frame.rotation;
+    
+    pose = ignition::math::Pose3d(pos.tX, pos.tY, pos.tZ,
+				  quat.w(), quat.x(), quat.y(), quat.z());
+    
+    return true;
 }
 
 bool GazeboYarpSkin::RetrieveLinksFromLocalNames(const std::vector<std::string> &linksLocalNames,
@@ -256,42 +341,111 @@ bool GazeboYarpSkin::ConfigureAllContactSensors()
     return true;
 }
 
+bool GazeboYarpSkin::RetrieveHandPose(ignition::math::Pose3d &pose)
+{
+    // Get current value of encoders
+    int nEncs;
+    bool ok = m_iEnc->getAxes(&nEncs);
+    if(!ok)
+	return false;
+
+    yarp::sig::Vector encs(nEncs);
+    ok = m_iEnc->getEncoders(encs.data());
+    if(!ok)
+	return false;
+
+    // Instantiate an arm chain
+    iCub::iKin::iCubArm arm(m_whichHand);
+
+    // Set the current values of the joints
+    arm.setAng(encs);
+
+    // Get pose of the hand
+    // as x,y,z + axis/angle
+    yarp::sig::Vector poseVector;
+    bool axisAngleRepr = true;
+    poseVector = arm.EndEffPose(axisAngleRepr);
+
+    // Convert attitude to a quaternion
+    yarp::math::Quaternion quat;
+    quat.fromAxisAngle(poseVector.subVector(3,6));
+
+    // Convert to a ignition::math::Pose3d
+    pose = ignition::math::Pose3d(poseVector[0], poseVector[1], poseVector[2],
+				  quat.w(), quat.x(), quat.y(), quat.z());
+
+    return true;
+}
+	
 void GazeboYarpSkin::OnWorldUpdate()
 {
+    // Retrieve hand pose
+    ignition::math::Pose3d handPose;
+    if (!RetrieveHandPose(handPose)) {
+        yError() << "GazeboYarpSkin::OnWorldUpdate error:"
+		 << "unable to get the pose of the"
+		 << m_whichHand
+		 << "hand";
+        return;
+    }
+
+    // Compose transformation from inertial to robot root frame
+    // and transformation from robot root frame to hand frame
+    ignition::math::Pose3d inertialToHand = m_inertialToRobot + handPose;
+    
     // Process contacts for each contact sensor
+    iCub::skinDynLib::skinContactList &skinContactList = m_portSkin.prepare();
+    skinContactList.clear();
+    
     for (size_t i=0; i<m_contactSensors.size(); i++)
     {
 	ContactSensor &sensor = m_contactSensors[i];
 	msgs::Contacts contacts = sensor.sensor->Contacts();
 	
-	// Only geometric center of the contact is considered
-	// in this implementation
-	// yarp::sig::Vector null(3, 0.0);
-	// iCub::skinDynLib::dynContact(sensor.bodyPart,
-	// 			     static_cast<int>(sensor.linkNumber),
-	// 			     null);
-	// iCub::skinDynLib::skinContact(
+	// Skip to next sensor if no contacts
 	if (contacts.contact_size() == 0)
 	    continue;
 	
-	yInfo() << "*****************************************";
-	yInfo() << "*" << sensor.sensorName;
-	yInfo() << "# contacts" << contacts.contact_size();
-	yInfo() << "*****************************************";
-
 	for (size_t j=0; j<contacts.contact_size(); j++)
 	{
 	    for (size_t k=0; k<contacts.contact(j).position_size(); k++)
 	    {
-		yInfo() << "(" << k << ")" << "Position:";
+		// Extract position from message
 		auto position = contacts.contact(j).position(k);
-		yInfo() << position.x() << position.y() << position.z();
+
+		// Convert to a pose with no rotation
+		ignition::math::Pose3d point(position.x(), position.y(), position.z(),
+					     0, 0, 0);
+
+		// Find the vector from the center of the palm of the hand
+		// to the contact point expressed in the frame attached to the
+		// palm of the hand (as done in the skinManager)
+		ignition::math::Pose3d diff = point - inertialToHand;
+
+		
+		// One skinContact for each position is considered
+		// TODO: averaging may be an idea
+		
+		// Only geometric center of the contact is considered
+		// in this implementation
+		ignition::math::Vector3d & diffPos = diff.Pos();
+		yarp::sig::Vector diffVector(3, 0.0);
+		diffVector[0] = diffPos.X();
+		diffVector[1] = diffPos.Y();
+		diffVector[2] = diffPos.Z();		
+		iCub::skinDynLib::dynContact dynContact(sensor.bodyPart,
+						     static_cast<int>(sensor.linkNumber),
+						     diffVector);
+		iCub::skinDynLib::skinContact skinContact(dynContact);
+		skinContact.setSkinPart(sensor.skinPart);
+
+		// Add contact to the list
+		skinContactList.push_back(skinContact);
 	    }
-
 	}
-	yInfo() << "*****************************************";
-	yInfo() << "";
     }
+    // Send data over port
+    m_portSkin.write();
 }
-
+    
 }
