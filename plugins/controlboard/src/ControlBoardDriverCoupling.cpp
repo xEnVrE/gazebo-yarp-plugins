369a3b716cfa45e6ce8d1571bc4721ee515bbd88
Nicola Piga <nicolapiga@gmail.com>
2018-07-22 18:00:23 +0200

    fix decoupling of fingers abduction

--- plugins/controlboard/src/ControlBoardDriverCoupling.cpp
+++ plugins/controlboard/src/ControlBoardDriverCoupling.cpp
@@ -464,14 +464,15 @@ FingersAbductionCouplingHandler::FingersAbductionCouplingHandler(gazebo::physics
 bool FingersAbductionCouplingHandler::decouplePos (yarp::sig::Vector& current_pos)
 {
     if (m_coupledJoints.size()!=m_couplingSize) return false;
-    current_pos[m_coupledJoints[0]] = current_pos[m_coupledJoints[3]];
+    double sum_pos = -current_pos[m_coupledJoints[0]] + current_pos[m_coupledJoints[2]] + current_pos[m_coupledJoints[3]];
+    current_pos[m_coupledJoints[0]] = 60.0 - sum_pos;
     return true;
 }
 
 bool FingersAbductionCouplingHandler::decoupleVel (yarp::sig::Vector& current_vel)
 {
     if (m_coupledJoints.size()!=m_couplingSize) return false;
-    current_vel[m_coupledJoints[0]] = current_vel[m_coupledJoints[3]];
+    current_vel[m_coupledJoints[0]] = current_vel[m_coupledJoints[3]] * 3;
     return true;
 }
 
@@ -479,7 +480,7 @@ bool FingersAbductionCouplingHandler::decoupleAcc (yarp::sig::Vector& current_ac
 {
 
     if (m_coupledJoints.size()!=m_couplingSize) return false;
-    current_acc[m_coupledJoints[0]] = current_acc[m_coupledJoints[3]];
+    current_acc[m_coupledJoints[0]] = current_acc[m_coupledJoints[3]] * 3;
     return true;
 }
 
@@ -489,19 +490,23 @@ bool FingersAbductionCouplingHandler::decoupleTrq (yarp::sig::Vector& current_tr
     return false;
 }
 
-bool FingersAbductionCouplingHandler::decouplePosLimits (std::vector<Range> &pos_limits) // NOT IMPLEMENTED
+bool FingersAbductionCouplingHandler::decouplePosLimits (std::vector<Range> &pos_limits)
 {
-    return false;
+    if (m_coupledJoints.size() != m_couplingSize) return false;
+    pos_limits[m_coupledJoints[0]].min = pos_limits[m_coupledJoints[0]].min + pos_limits[m_coupledJoints[2]].min + pos_limits[m_coupledJoints[3]].min;
+    pos_limits[m_coupledJoints[0]].max = pos_limits[m_coupledJoints[0]].max + pos_limits[m_coupledJoints[2]].max + pos_limits[m_coupledJoints[3]].max;
+    return true;
 }
 
 yarp::sig::Vector FingersAbductionCouplingHandler::decoupleRefPos (yarp::sig::Vector& pos_ref)
 {
     yarp::sig::Vector out = pos_ref;
     if (m_coupledJoints.size()!=m_couplingSize) {yError() << "FingersAbductionCouplingHandler: Invalid coupling vector"; return out;}
-    out[m_coupledJoints[0]] = -pos_ref[m_coupledJoints[0]]/2;
+    double ref = 60.0 - pos_ref[m_coupledJoints[0]];
+    out[m_coupledJoints[0]] = -ref/3;
     out[m_coupledJoints[1]] = 0.0;
-    out[m_coupledJoints[2]] = pos_ref[m_coupledJoints[0]]/2;
-    out[m_coupledJoints[3]] = pos_ref[m_coupledJoints[0]];
+    out[m_coupledJoints[2]] = ref/3;
+    out[m_coupledJoints[3]] = ref/3;
     return out;
 }
 
@@ -509,10 +514,10 @@ yarp::sig::Vector FingersAbductionCouplingHandler::decoupleRefVel (yarp::sig::Ve
 {
     yarp::sig::Vector out = vel_ref;
     if (m_coupledJoints.size()!=m_couplingSize) {yError() << "FingersAbductionCouplingHandler: Invalid coupling vector"; return out;}
-    out[m_coupledJoints[0]] = -vel_ref[m_coupledJoints[0]]/2;
+    out[m_coupledJoints[0]] = -vel_ref[m_coupledJoints[0]]/3;
     out[m_coupledJoints[1]] = 0.0;
-    out[m_coupledJoints[2]] = vel_ref[m_coupledJoints[0]]/2;
-    out[m_coupledJoints[3]] = vel_ref[m_coupledJoints[0]];
+    out[m_coupledJoints[2]] = vel_ref[m_coupledJoints[0]]/3;
+    out[m_coupledJoints[3]] = vel_ref[m_coupledJoints[0]]/3;
     return out;
 }
 
@@ -520,10 +525,10 @@ yarp::sig::Vector FingersAbductionCouplingHandler::decoupleRefTrq (yarp::sig::Ve
 {
     yarp::sig::Vector out =trq_ref;
     if (m_coupledJoints.size()!=m_couplingSize) {yError() << "FingersAbductionCouplingHandler: Invalid coupling vector"; return out;}
-    out[m_coupledJoints[0]] = -trq_ref[m_coupledJoints[0]]/2;
+    out[m_coupledJoints[0]] = -trq_ref[m_coupledJoints[0]]/3;
     out[m_coupledJoints[1]] = 0.0;
-    out[m_coupledJoints[2]] = trq_ref[m_coupledJoints[0]]/2;
-    out[m_coupledJoints[3]] = trq_ref[m_coupledJoints[0]];
+    out[m_coupledJoints[2]] = trq_ref[m_coupledJoints[0]]/3;
+    out[m_coupledJoints[3]] = trq_ref[m_coupledJoints[0]]/3;
     return out;
 }
 
